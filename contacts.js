b095a8dc376096e3a09890c3f069d2226820b15b
Oleksandr-Nehoda <iamsoul@ukr.net>
2023-01-17 00:46:27 +0200

    start

---
+++ contacts.js
@@ -0,0 +1,6 @@
+const path = require('path');
+const fs = require('fs').promises;
+
+const contactsPath = path.resolve('./db/contcts.json');
+
+// console.log(`LOG: ${path.resolve('./db/contcts.json')}`);
