54499d0c9d450aba57050c47c6d0da1a6b228b9c
Oleksandr-Nehoda <iamsoul@ukr.net>
2023-01-17 16:23:05 +0200

    start

--- contacts.js
+++ contacts.js
@@ -1,6 +1,34 @@
 const path = require('path');
 const fs = require('fs').promises;
 
-const contactsPath = path.resolve('./db/contcts.json');
+const contactsPath = path.resolve('./db/contacts.json');
 
 // console.log(`LOG: ${path.resolve('./db/contcts.json')}`);
+
+async function listContacts () {
+    try {
+    const data = await fs.readFile(contactsPath, 'utf-8');
+    const contacts = JSON.parse(data);
+    return contacts;
+} catch (err) {
+console.error(err);
+}
+    } ;
+console.log(listContacts());
+
+
+ const getContactById = async (contactId) => {
+    try {
+        const contacts = await listContacts();
+        const contactById = contacts.find(item => item.id === String(contactId));
+        if(!contactById) {
+            return null;
+        }
+        return contactById;
+    } catch (err) {
+        console.error(err); 
+    }
+    } ;   
+    console.log(`LOGById: ${getContactById(1)}`);
+    
+    
